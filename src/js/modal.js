e64b884767f1a4f91aaea7a0b42c6b9636ae2f95
Muhammed-Hagras <40708938+Muhammed-Hagras@users.noreply.github.com>
2023-06-03 22:21:03 +0200

    refactor ti MVC Arch

---
+++ src/js/modal.js
@@ -0,0 +1,30 @@
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    //Fetch recipe
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    // console.log(res, data);
+
+    let { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      source_url: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+};
